aaf00dd3dfbd1db4bfcebaedb08e2d2f94a6b5fe
Irene <irenemanningm@gmail.com>
2024-02-22 17:28:35 -0600

    Add profile image to Wuser table with active storage has_attatched.

--- app/models/wuser.rb
+++ app/models/wuser.rb
@@ -1,4 +1,5 @@
 class Wuser < ApplicationRecord
+    has_one_attached :profile_image
     has_many :reviews
     has_many :wines, through: :reviews
     has_secure_password
--- app/serializers/wuser_serializer.rb
+++ app/serializers/wuser_serializer.rb
@@ -1,6 +1,12 @@
 class WuserSerializer < ActiveModel::Serializer
-  attributes :id, :username, :image_url
+  attributes :id, :username, :profile_image
   
   has_many :wines
   has_many :reviews
+
+  include Rails.application.routes.url_helpers
+  def profile_image
+    rails_blob_path(object.profile_image, only_path: true) if object.profile_image.attached?
+  end
+  
 end
